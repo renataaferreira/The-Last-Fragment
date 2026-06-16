4eee8af5d8e6e1a570072b6d3b82911b61e62b6b
renataaferreira <ferreira.renata@escolar.ifrn.edu.br>
2026-06-16 09:44:13 -0300

    o personagm principal no jogo
    o dano ainda esta com problema

--- The Last Fragment/Assets/Scripts/TomarDano.cs
+++ The Last Fragment/Assets/Scripts/TomarDano.cs
@@ -4,12 +4,17 @@ public class Dano : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Debug.Log("TRIGGER FUNCIONOU!");
+
         if (collision.CompareTag("Player"))
         {
+            Debug.Log("PLAYER ENCONTROU O BOSS!");
+
             VidaPlayer vida = collision.GetComponent<VidaPlayer>();
 
             if (vida != null)
             {
+                Debug.Log("DANO APLICADO!");
                 vida.TomarDano(10);
             }
         }
