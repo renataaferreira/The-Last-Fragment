6caa0ce485b12d23815eada5455e4bf7c4b44634
renataaferreira <ferreira.renata@escolar.ifrn.edu.br>
2026-06-25 11:52:29 -0300

    cheekpoit
    esta com defeito

--- The Last Fragment/Assets/Scripts/CheekPoint.cs
+++ The Last Fragment/Assets/Scripts/CheekPoint.cs
@@ -4,15 +4,19 @@ public class Checkpoint : MonoBehaviour
 {
     private bool ativado = false;
 
-    void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player") && !ativado)
+        if (ativado)
+            return;
+
+        if (collision.CompareTag("Player"))
         {
             Player player = collision.GetComponent<Player>();
 
             if (player != null)
             {
                 player.SetCheckpoint(transform);
+
                 ativado = true;
 
                 Debug.Log("Checkpoint ativado!");
