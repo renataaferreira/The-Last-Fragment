25c329aab1fdc9005f73192f6855465357d49705
renataaferreira <ferreira.renata@escolar.ifrn.edu.br>
2026-08-18 08:22:55 -0300

    .
    .

--- The Last Fragment/Assets/Scripts/ContadorFragmentos.cs
+++ The Last Fragment/Assets/Scripts/ContadorFragmentos.cs
@@ -1,16 +1,34 @@
 using UnityEngine;
+using TMPro;
 
 public class ContadorFragmentos : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static ContadorFragmentos Instancia;
+
+    [SerializeField] private TextMeshProUGUI textoFragmentos;
+
+    private int fragmentosColetados = 0;
+    private int totalFragmentos = 3;
+
+    private void Awake()
     {
-        
+        Instancia = this;
+        AtualizarTexto();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ColetarFragmento()
     {
-        
+        fragmentosColetados++;
+        AtualizarTexto();
+
+        Debug.Log("Fragmentos coletados: " + fragmentosColetados);
+    }
+
+    private void AtualizarTexto()
+    {
+        textoFragmentos.text = "FRAGMENTOS: " +
+                               fragmentosColetados +
+                               "/" +
+                               totalFragmentos;
     }
 }
